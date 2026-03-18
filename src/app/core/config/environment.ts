f296a54563074f42347fcd5cbe936b4fc1cf1f3d
Jeison ortiz <ojeison21@gmail.com>
2026-03-18 14:36:10 -0500

    en produccion

--- src/app/core/config/environment.ts
+++ src/app/core/config/environment.ts
@@ -3,16 +3,16 @@
  * Cambia aquí la URL del backend según el ambiente
  */
 
-export const environment = {
-  production: false,
-  apiUrl: 'https://ms-backend-r8lr.onrender.com/api/v1', // Cambia aquí para desarrollo/producción
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://api.mstucasa.com/api/v1', // Cambia aquí para desarrollo/producción
+// };
 
 /**
  * Configuración para producción
  * Descomenta y usa cuando despliegues a producción
  */
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://tu-dominio.com/api/v1',
-// };
+export const environment = {
+   production: true,
+  apiUrl: 'https://api.mstucasa.com/api/v1',
+};
