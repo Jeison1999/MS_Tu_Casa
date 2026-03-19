f3b14aacc4e8c6779df9373d340abed60076f63c
Jeison ortiz <ojeison21@gmail.com>
2026-03-19 14:48:20 -0500

    deploy

--- src/app/core/config/environment.ts
+++ src/app/core/config/environment.ts
@@ -3,16 +3,16 @@
  * Cambia aquí la URL del backend según el ambiente
  */
 
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/v1', // Cambia aquí para desarrollo/producción
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api/v1', // Cambia aquí para desarrollo/producción
+// };
 
 /**
  * Configuración para producción
  * Descomenta y usa cuando despliegues a producción
  */
-// export const environment = {
-//    production: true,
-//   apiUrl: 'https://api.mstucasa.com/api/v1',
-// };
+export const environment = {
+   production: true,
+  apiUrl: 'https://api.mstucasa.com/api/v1',
+};
